b6a4e52c84e28f11c28cee37a9e7269c81c52aef
Yi Ning <yining@gmail.com>
2013-03-09 00:05:44 +0800

    Initial Commit

---
+++ MyAdventureGame/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MyAdventureGame
+//
+//  Created by yining on 3/9/13.
+//  Copyright (c) 2013 yining. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
